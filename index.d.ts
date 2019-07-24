c651abe69a256c067c40530d9d7c53f8bccbfddc
John Carmichael <carmichael.john@aol.com>
2019-07-24 22:50:55 +0200

    type file added

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Option object to control this package
+ */
+interface Options {
+  /**
+   * Bool determine to keep the keys missing from the input but present in the map but only with values of null
+   */
+  keepKeys: boolean,
+}
+export default function(input: object, map: object, options: Options): object
